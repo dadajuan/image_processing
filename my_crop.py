c7e641233c7512a373df79a018016287c0f90b4d
dadajuan <58908110+dadajuan@users.noreply.github.com>
2023-03-03 19:21:58 +0800

    Add files via upload

--- my_crop.py
+++ my_crop.py
@@ -6,11 +6,11 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def crop(input_img_path, output_img_path):
     image = Image.open(input_img_path)
-    image_crop = image.resize((876,657))
+    image_crop = image.resize((128,128))
     image_crop.save(output_img_path)
 
-dataset_dir = 'D:/浏览器下载/valid'
-output_dir = 'D:/浏览器下载/valid768x576'
+dataset_dir = 'D:/Desktop/G1-6PIP/'
+output_dir = 'D:/Desktop/G1-6PIP2/'
 
 #获得需要转化的图片路径并生成目标路径
 image_filenames = [(os.path.join(dataset_dir, x), os.path.join(output_dir, x))
